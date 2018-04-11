aa656c57cf84421e8810fbb4c0d1a3190910ffb4
Madeline Watson <mcwatson91@gmail.com>
2018-04-11 11:36:04 -0400

    hide text field

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 // when the page loads give focus to the first text field
   document.getElementById("name").focus();
+
+// hide the "other-title" text field
+// if "other" option is selected, show text field
+
+$("#other-title").hide();
