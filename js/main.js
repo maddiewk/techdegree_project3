645d24392d577df574737fd9e926b3f7b63b28ee
Madeline Watson <mcwatson91@gmail.com>
2018-04-12 10:39:03 -0400

    fiddling with console.log to try to fix the color menu function

--- js/main.js
+++ js/main.js
@@ -16,24 +16,39 @@ $("#title").change(function() {
 });
 
 // function to hide or display color menus depending on user's selection
-$("#design").change(function() {
-  const cornflowerBlue = $("#color option:eq(0)");
-  const slateGrey = $("#color option:eq(1)");
-  const gold = $("#color option:eq(2)");
-  const tomato = $("#color option:eq(3)");
-  const steelBlue = $("#color option:eq(4)");
-  const dimGrey = $("#color option:eq(5)");
+// $("#design").change(function() {
+//   const cornflowerBlue = $("#color option[value='cornflowerblue']");
+//   const slateGrey = $("#color option:eq(1)");
+//   const gold = $("#color option:eq(2)");
+//   const tomato = $("#color option:eq(3)");
+//   const steelBlue = $("#color option:eq(4)");
+//   const dimGrey = $("#color option:eq(5)");
+//
+//   var $selectColor = $("#design option:selected").text();
+//
+//   if( $selectColor === "Theme - JS Puns" ) {
+//     tomato.hide();
+//     steelBlue.hide();
+//     dimGrey.hide();
+//     cornflowerBlue.show();
+//     slateGrey.show();
+//     gold.show();
+//   } else if ( $selectColor === "Theme - I &#9829; JS" ) {
+//     cornflowerBlue.hide();
+//     slateGrey.hide();
+//     gold.hide();
+//     tomato.show();
+//     steelBlue.show();
+//     dimGrey.show();
+//   }
+//
+// });
 
-  var $selectColor = $("#design option:selected").text();
 
-  if( $selectColor === "Theme - JS Puns" ) {
-    tomato.hide();
-    steelBlue.hide();
-    dimGrey.hide();
-  } else if ( $selectColor === "Theme - I &#9829; JS" ) {
-    cornflowerBlue.hide();
-    slateGrey.hide();
-    gold.hide();
-  }
 
+
+
+
+$("#color").each(function(index) {
+  console.log(index + ": " + $(this).text());
 });
