c9a8efb6a8996f13b09d02acaad00feef53c67a2
Madeline Watson <mcwatson91@gmail.com>
2018-04-11 22:10:11 -0400

    spacing

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 // when the page loads give focus to the first text field
-  document.getElementById("name").focus();
+document.getElementById("name").focus();
 
 // hide the "other-title" text field
 // if "other" option is selected, show text field
 
-  $("#other-title").hide();
+$("#other-title").hide();
 
 $("#title").change(function() {
   var $selected = $("#title option:selected").text();
-  if( $selected === "Other") {
-    $("#other-title").show();
+    if( $selected === "Other") {
+      $("#other-title").show();
   } else {
-    $("#other-title").hide();
+      $("#other-title").hide();
   }
-  });
+});
