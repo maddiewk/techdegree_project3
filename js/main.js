1156c3e2af9ecd5c131dd21b663dc6ed5b62ad68
Madeline Watson <mcwatson91@gmail.com>
2018-04-12 15:37:50 -0400

    color menu function works now

--- js/main.js
+++ js/main.js
@@ -24,16 +24,17 @@ $("#title").change(function() {
   $("#color option[value='steelblue']").remove();
   $("#color option[value='dimgrey']").remove();
   $("#color").append("<option value='select'>Please select a T-shirt theme</option>");
-  
+
 $("#design").change(function() {
 
   const puns = $("#design option[value='js puns']").text();
   const heart = $("#design option[value='heart js']").text();
   var $selectColor = $("#design option:selected").text();
   const message = "<option value='select'>Please select a T-shirt theme</option>";
+    $("#color").append(message);
 
   $("#color").children().remove();
-  $("#color").append(message);
+
 
 
   if( $selectColor === puns ) {
@@ -46,8 +47,8 @@ $("#design").change(function() {
     $("#color").append("<option value='steelblue'>Steel Blue</option>");
     $("#color").append("<option value='dimgrey'>Dim Grey</option>");
   }
-  // if($selectColor === "Select Theme") {
-  //   $("#color").append("<option value='select'>Please select a T-shirt theme</option>");
-  // }
+  if ($selectColor === "Select Theme") {
+    $("#color").append(message);
+  }
 
 });
