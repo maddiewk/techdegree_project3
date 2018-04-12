d8df2d70a4122f9b533ae6c9c9b88671441bf11f
Madeline Watson <mcwatson91@gmail.com>
2018-04-12 10:21:06 -0400

    completed function to show/hide color menus

--- js/main.js
+++ js/main.js
@@ -16,6 +16,24 @@ $("#title").change(function() {
 });
 
 // function to hide or display color menus depending on user's selection
-function displayColorMenu() {
+$("#design").change(function() {
+  const cornflowerBlue = $("#color option:eq(0)");
+  const slateGrey = $("#color option:eq(1)");
+  const gold = $("#color option:eq(2)");
+  const tomato = $("#color option:eq(3)");
+  const steelBlue = $("#color option:eq(4)");
+  const dimGrey = $("#color option:eq(5)");
 
-}
+  var $selectColor = $("#design option:selected").text();
+
+  if( $selectColor === "Theme - JS Puns" ) {
+    tomato.hide();
+    steelBlue.hide();
+    dimGrey.hide();
+  } else if ( $selectColor === "Theme - I &#9829; JS" ) {
+    cornflowerBlue.hide();
+    slateGrey.hide();
+    gold.hide();
+  }
+
+});
