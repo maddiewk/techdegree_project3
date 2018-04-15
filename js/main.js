b5a1fd10cbca7a5ea3b081ca4cc5636bfe09cfdb
Madeline Watson <mcwatson91@gmail.com>
2018-04-15 15:20:53 -0400

    each function to assign each label an index number

--- js/main.js
+++ js/main.js
@@ -57,6 +57,13 @@ function disableCheckbox() {
   const $library = $(".activities input[name='js-libs']");
   const $node = $(".activities input[name='node']");
 
+
+$(".activities label").each (function(index, text) {
+  console.log(index + ": " + $(this).text());
+});
+
+
+
   $(".activities :checkbox").change(function() {
 
 // disable/enable options 2 and 4 when either is checked
